c09fa27077bd3c9794fb2e54be7d9185c83264d5
Michael Bruce <me@michaelbruce.co>
2019-09-19 22:42:02 +0100

    foundational commit; ready to run with C-c M-j

---
+++ src/resume/core.cljs
@@ -0,0 +1,13 @@
+(ns resume.core
+  (:require [reagent.core :as reagent]))
+
+(enable-console-print!)
+
+(println "alright!")
+
+(defonce state (reagent/atom {}))
+
+(defn title []
+  [:h3 "Nutrack"])
+
+(reagent/render [title] (.getElementById js/document "app"))
