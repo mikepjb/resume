6f0cafbd6afd0cb6b4db17141d507465d1058af0
Michael Bruce <me@michaelbruce.co>
2019-09-19 23:20:20 +0100

    include github link

--- src/resume/core.cljs
+++ src/resume/core.cljs
@@ -7,15 +7,30 @@
 
 (defonce state (reagent/atom {}))
 
-(def mail-icon [:svg
- {:class "icon-mail", :viewBox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
- [:path
-  {:class "primary",
-   :d
+(def mail-icon
+  [:svg
+   {:class "icon-mail", :viewBox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
+   [:path
+    {:class "primary",
+     :d
      "M22 8.62V18a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V8.62l9.55 4.77a1 1 0 0 0 .9 0L22 8.62z"}]
- [:path
-  {:class "secondary",
-   :d "M12 11.38l-10-5V6c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v.38l-10 5z"}]])
+   [:path
+    {:class "secondary",
+     :d "M12 11.38l-10-5V6c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v.38l-10 5z"}]])
+
+(def id-icon
+  [:svg
+   {:class "icon-identification",
+    :viewBox "0 0 24 24",
+    :xmlns "http://www.w3.org/2000/svg"}
+   [:path
+    {:class "primary",
+     :d
+     "M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2zm13 3a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2h-2zm-2 4a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2h-4zm1 4a1 1 0 0 0 0 2h3a1 1 0 0 0 0-2h-3z"}]
+   [:path
+    {:class "secondary",
+     :d
+     "M8 12a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm-2 2h4a2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-1c0-1.1.9-2 2-2z"}]])
 
 (defn title []
   [:div
@@ -27,7 +42,9 @@
   "tiny links/icons that sit in the top right of my CV."
   []
   [:div
-   [:div "me@michaelbruce.co" mail-icon]])
+   [:div "me@michaelbruce.co" mail-icon]
+   [:div [:a {:href "https://github.com/mikepjb"} "https://github.com/mikepjb"] id-icon]
+   ])
 
 ;; (defn )
 
