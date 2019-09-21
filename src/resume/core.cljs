256fae9f733f102e092188e46cc7dfeefdd87c88
Michael Bruce <me@michaelbruce.co>
2019-09-21 22:35:58 +0100

    shorten display of summary links

--- src/resume/core.cljs
+++ src/resume/core.cljs
@@ -15,9 +15,9 @@
   []
   [:div.summary-links
    [:div "me@michaelbruce.co" svg/mail]
-   [:div [:a {:href "https://github.com/mikepjb"} "https://github.com/mikepjb"] svg/id]
+   [:div [:a {:href "https://github.com/mikepjb"} "github.com/mikepjb"] svg/id]
    [:div [:a {:href "07714030087"} "07714030087"] svg/phone]
-   [:div [:a {:href "https://www.linkedin.com/in/mikepjb"} "https://www.linkedin.com/in/mikepjb"] svg/id]
+   [:div [:a {:href "https://www.linkedin.com/in/mikepjb"} "linkedin.com/in/mikepjb"] svg/id]
 
    ;; 
    [:div "Sevenoaks, England" svg/location]])
