0939bf4d0dc168d9456e54eb5010895f90bc8296
Michael Bruce <me@michaelbruce.co>
2019-09-22 17:48:47 +0100

    final draft (pdf created via chrome)

--- src/resume/core.cljs
+++ src/resume/core.cljs
@@ -2,13 +2,10 @@
   (:require [reagent.core :as reagent]
             [resume.svg :as svg]))
 
-;; talk about what I've done
-;; start every bullet point with a verb "doing"
-
 (defonce state (reagent/atom {}))
 
 (defn title []
-  [:div {:style {:width "65%"}}
+  [:div {:style {:width "60%"}}
    [:h3 "Michael Bruce"]
    [:h5 "Software Engineer"]
    [:p "A resourceful software enthusiast with 6 years of experience in software development. Keen to provide business value and deliver things that matter."]])
@@ -32,10 +29,10 @@
 (defn experience-header
   [title company-name from to location]
   [:div.experience-header
-   [:div ;; {:style {:margin-right 100}}
+   [:div
     [:h5 title]
     [:h6 company-name]]
-   [:div {:style {:text-align "right"}}
+   [:div {:style {:text-align "right" :font-size "10px" :color "#555"}}
     [:div (str from " - " to)]
     [:div location]]])
 
@@ -61,53 +58,51 @@
      [column-section "Work Experience"]
      [experience-header "Software Engineer" "Mercury Holidays" "06/2019" "Present" "Sevenoaks, Kent"]
      [detail
-      "Building a data-centric backend system for serving new technology at the company."
+      "Built a backend service to collate and serve information from legacy systems to the frontend."
+      "Introduced grpc/swagger documented API to improve service visibility."
       "Designed data models and diagrammed this to communicate progress in the team and wider company."
-      "Search index in algolia.."
-      "Coordinating a team of 3 to deliver.."
-      "...led? raising monitoring and visibility on the project with webhooks & dashboards."]
-     [experience-header "Developer" "Strategic Blue" "02/2017" "06/2019" "London Bridge, London"]
+      "Improved reliability for search indexes in Algolia."]
+     [experience-header "Developer" "Strategic Blue" "02/2017" "06/2019" "London Bridge"]
      [detail
-      "Rallied together with a team of 6 developers to rewrite (and extensively test) our billing engine, to greatly reduce cost of maintenance."
-      "Brought a data science project in R to production, wrapped in a yada web server."
+      "Communicated to keep things running smoothly and provide visibility on how our technology is used."
+      "Created a new billing engine (with extensive testing!), to greatly reduce cost of maintenance."
+      "Brought a data science project in R to production, wrapped in a Yada-based web server."
       "Improved report generation by using wkhtmltopdf over LaTeX."
-      "Responsible for management & maintenance of a set of Ruby microservices."
-      "Imp"]
-     [experience-header "Developer" "Singletrack" "04/2014" "02/2017" "Old Street, London"]
+      "Responsible for management & maintenance of a set of Ruby microservices."]
+     [experience-header "Developer" "Singletrack" "04/2014" "02/2017" "Old Street"]
      [detail
       "Responsible for ensuring portal could be rolled out in China with CI/CD and be accessible across the world."
       "Created a failover utility for changing email marketing networks."
       "Worked directly with vendors to ensure client research could be distributed."
-      "Worked directly with clients to ensure their data was correctly loaded."]
-     [experience-header "Technical Analyst" "M&C Saatchi Mobile" "09/2013" "02/2014" "Leicester Square, London"]
+      "Worked directly with clients to ensure their data was correctly loaded."
+      "Improved performance of a watermarking microservice to reduce cost."]
+     [experience-header "Technical Analyst" "M&C Saatchi Mobile" "09/2013" "02/2014" "Leicester Square"]
      [detail
       "Responsible for providing unique tracking IDs for advertisments, collating metrics and reporting back to multiple teams."]]
     [:div.column
      [column-section "Open Source & Projects"]
      [project-header "tpope/fireplace.vim" "https://github.com/tpope/vim-fireplace/pull/365"]
-     [detail "Made PR (with Dominic) to allow Java source code jumping."]
+     [detail "Enabled Java source code jumping."]
      [project-header "juxt/pack.alpha" "https://github.com/juxt/pack.alpha/pull/60"]
      [detail "Removed redundant map now provided by tools.deps.alpha."]
      [project-header "nutrack" "https://github.com/mikepjb/nutrack"]
-     [detail "Clojure(script) application to improve my stack knowledge in the Clojure world (and to have some fun). Sends metrics to prometheus, from the app and Postgres."]
+     [detail "Clojure(script) application to improve my stack knowledge in the Clojure world (and to have some fun)."]
      [project-header "go-nutrack" "https://github.com/mikepjb/nutrack" "https://nutrack.michaelbruce.co"]
      [detail "A personal project in my spare time to improve my abilities fullstack, including UX and frontend work. The idea is to help people get more visibility on what nutrition they are getting and how expensive it is when food shopping."]
-     [project-header "sysconf"]
-     [detail "sysconf does 3 things, it can create a personalised live Arch image, it can install Arch linux with a set of configured packages (via json) and keep your setup clean/up to date (again via json)."]
-     [project-header "citadel"]
-     [detail "A lighter version of sysconf, allowing configuration via edn (I have a system.edn file that syncs packages between machines)."]
+     [project-header "sysconf" "https://github.com/mikepjb/sysconf"]
+     [detail "Creates a customised boot image, installs fresh Arch Linux (with configurable packages), keeps installation clean and updated."]
+     [project-header "citadel" "https://github.com/mikepjb/citadel"]
+     [detail "A lighter version of sysconf, allowing configuration via edn."]
      [column-section "Education"]
-     [experience-header "Computer Science BSc, 1st Class" "The University of Hertfordshire" "09/2009" "06/2013" "Hatfield, Hertfordshire"]
-     [experience-header "3 A Level, 1 AS Level" "Weald of Kent Grammar School" "09/2007" "06/2009" "Tonbridge, Kent"]
+     [experience-header "Computer Science BSc" "The University of Hertfordshire" "09/2009" "06/2013" "Hertfordshire"]
+     [detail "1st Class, also ran the Discussion Society"]
+     [experience-header "3 A Levels, 1 AS Level" "Weald of Kent Grammar School" "09/2007" "06/2009" "Tonbridge, Kent"]
+     [detail "Maths, Physics, Chemistry, Biology"]
      [column-section "Skills"]
      [detail
-      "An adept Emacs & Vim user"
-      "Used Arch Linux for years (familiar with unix conventions and bash)"
-      "Ruby 3 years"
-      "Clojure 2.5 years (familiar with which libraries?)"
-      "AWS Cloud Practictioner - used for ages"
-      "Postgres"
-      "UX basics (refactoring ui/uxmovement)"]
+      "Adept Emacs & Vim user"
+      "Familiar with Clojure(script) (2.5 years), Ruby (5 years), Arch Linux (5+ years), AWS (5+ years), PostgreSQL (5+ years) and MongoDB (2.5+ years)"
+      "UX and usability design basics."]
      ]]
    ])
 
